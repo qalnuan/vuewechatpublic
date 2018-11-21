50859edae7fa12974b195e8212535211dd677bd2
xiongxiangquan <xiongxq@landicorp.com>
2018-11-21 21:23:51 +0800

    首页布局完成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
 <el-container>
-  <el-header style="height:24px;">
+  <el-header style="height:24px;margin-bottom:5px;">
     <el-row :gutter="20" style="height:40px;">
       <el-col :span="10" style="height:40px;">
         <div style="height:100%;">
@@ -17,26 +17,33 @@
     </el-row>
   </el-header>
 
-  <el-main>
+  <el-main style="padding:2px">
     <el-carousel :interval="3000" arrow="hover">
       <el-carousel-item v-for="item in backImgs" :key="item">
         <img style="width:100%;height:100%" :src="item.url" />
         </el-carousel-item>
     </el-carousel>
     <el-row style="margin-top:12px;">
-      <el-col v-for="o in 2" :key="o">
+      <el-col v-for="o in 5" :key="o">
         <el-card :body-style="{ padding: '0px' }">
-          <img src="../assets/product.jpg" class="image">
-          <div style="position:absolute; z-index:2; background-color:rgba(0, 0, 0, 0.5); top:5px;color:white;">
-            销售量
-          </div>
-          <div style="padding: 14px;">
-            <span>好吃的汉堡</span>
-            <div class="bottom clearfix">
-              <time class="time">{{ currentDate }}</time>
-              <el-button type="text" class="button">操作按钮</el-button>
-            </div>
-          </div>
+          <el-row>
+            <el-col :span="10" style="margin-top: 12px;">
+              <div style="position:absolute; z-index:2; background-color:rgba(0, 0, 0, 0.5); color:white;">
+                销售量
+              </div>
+              <img src="../assets/product.jpg" class="image">
+            </el-col>
+              <el-col :span="14">
+                <div style="padding: 4px;">
+                  <div style="text-size:13px;">
+                    <span style="font-size:10px;">【福州网红火锅店】雄柒映象火锅~会跳舞的火锅店~鸳鸯子母锅~仅￥9.9元兑换100元菜金抵用券~火锅、小吃可以放心点啦~~</span>
+                  </div>
+                  <div style="color:orange;text-align:left;text-size:13px;">
+                    <span>¥9.9</span>
+                  </div>
+                </div>
+              </el-col>
+          </el-row>
         </el-card>
       </el-col>
     </el-row>
@@ -75,7 +82,13 @@ export default {
 
 .image {
   width: 100%;
+  height: 100%;
   display: block;
+  background-repeat: no-repeat;
+  background-size: cover;
+  -webkit-background-size: cover;
+  -o-background-size: cover;
+  background-position: center 0;
 }
 
 .el-carousel__item h3 {
